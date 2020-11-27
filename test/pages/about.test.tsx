f4c549c6126e0758db461f5773db0a31cec33e0f
daizpan <daizpan6@gmail.com>
2020-11-27 17:42:46 +0900

    Fix lint test files

--- test/pages/about.test.tsx
+++ test/pages/about.test.tsx
@@ -1,8 +1,8 @@
-import About from "src/pages/about";
-import { render } from "test/test-utils";
+import About from 'src/pages/about';
+import { render } from 'test/test-utils';
 
-describe("About page", () => {
-  it("matches snapshot", () => {
+describe('About page', () => {
+  it('matches snapshot', () => {
     const { asFragment } = render(<About />, {});
     expect(asFragment()).toMatchSnapshot();
   });
